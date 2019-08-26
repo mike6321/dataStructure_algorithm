315f0b54a0bf14894d86a985e6c2431f1674aba2
mike6321 <33277588+mike6321@users.noreply.github.com>
2019-08-27 00:10:42 +0900

    연습문제

--- Homework04/Problem05.py
+++ Homework04/Problem05.py
@@ -36,16 +36,24 @@ class BinaryTree:
         q = ArrayQueue()
         trav = []
 
+        #1. q에 루트를 add
         if self.root:
             q.enqueue(self.root)
 
+        #2. q가 빌때까지 루트를 돈다.
         while not q.isEmpty():
+            #3. 들어있는 큐 빼기
             node = q.dequeue()
+            #4. 뺀 큐 순회 배열에 append
             trav.append(node.data)
+
+            #양방향이기때문에 왼,오 나누어 큐에 집에 넣기
             if node.left:
                 q.enqueue(node.left)
             if node.right:
                 q.enqueue(node.right)
+
+            #이러한 식으로 계속 넣다뺏다들 반복한다음 큐가 들어있지 않다면 리턴한다. [거의 공식]
 
         return trav
 
